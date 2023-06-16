c55eddcc06688a0ad67c43a115c2ff2ba30849e8
Amaan1234567 <110830860+Amaan1234567@users.noreply.github.com>
2023-06-16 06:35:31 +0530

    added js for prediction stuff

--- website/js/prediction.js
+++ website/js/prediction.js
@@ -1,5 +1,16 @@
 
+
+
 document.getElementById('send-btn').addEventListener('click', async () => {
+    let data = new Array(132).fill(0)
+let symptoms = Array.from(document.getElementsByClassName('symptoms'))
+console.log(data)
+for (let i = 0; i < 30; i++) {
+    console.log(symptoms[i])
+    if (symptoms[i].checked==true) {
+        data[i] = 1
+    }
+}
     const response = await fetch("http://127.0.0.1:8000/predict", {
         method: 'POST',
         headers: {
@@ -10,11 +21,11 @@ document.getElementById('send-btn').addEventListener('click', async () => {
             'Content-Type': 'application/json'
         },
 
-        body:JSON.stringify({
-            "data": [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        body: JSON.stringify({
+            "data": data
         })
     })
 
-    const content = await response.json();    
+    const content = await response.json();
     document.getElementById("response").innerHTML = content['prediction'];
 })
